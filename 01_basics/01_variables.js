c1b4a34e6bdcc448fd0ce998c5d2680457d1e574
amitdevelopr <amitdevel47@gmail.com>
2024-05-09 00:15:53 +0530

    first commit

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId = 144453
+
+let accountEmail = "amit@google.com"
+var accountPassword = "12345"
+accountCity= "jamshedpur"
+
+// accountId = 2 //not allowed
+
+accountEmail = "amit2@techamit.in"
+accountPassword="232323"
+accountCity="patna"
+
+console.log(accountEmail);
+console.log(accountPassword);
+console.log(accountCity)
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
