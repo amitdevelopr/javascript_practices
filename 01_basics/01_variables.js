a8ae2e4179764620f64ba18ca5363850d7f0c9cd
amitdevelopr <amitdevel47@gmail.com>
2024-05-16 14:39:14 +0530

    data

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,17 +1,28 @@
-const accountId = 144453
+const accountId = 144453;
 
-let accountEmail = "amit@google.com"
-var accountPassword = "12345"
-accountCity= "jamshedpur"
-
+let accountEmail = "amit@google.com";
+var accountPassword = "12345";
+accountCity = "jamshedpur";
+let accountState;
 // accountId = 2 //not allowed
 
-accountEmail = "amit2@techamit.in"
-accountPassword="232323"
-accountCity="patna"
+accountEmail = "amit2@techamit.in";
+accountPassword = "232323";
+accountCity = "patna";
 
 console.log(accountEmail);
 console.log(accountPassword);
-console.log(accountCity)
+console.log(accountCity);
 
-console.table([accountId,accountEmail,accountPassword,accountCity])
+console.table([
+  accountId,
+  accountEmail,
+  accountPassword,
+  accountCity,
+  accountState,
+]);
+
+/*
+prefer not to use var 
+because it has issue with block scope and function scope.
+*/
