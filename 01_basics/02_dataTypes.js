4bb7faa21a2ed5152064fa71da968310e8315ebe
amitdevelopr <amitdevel47@gmail.com>
2024-05-16 14:37:06 +0530

    data type conversion

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,21 @@
+"use strict"  //treat all js code with latest function
+
+//alert(3+3) // we are using node js not browser
+
+console.log(4 + 4);
+console.log("Amit")
+
+
+let name="Amit"
+let age=30
+let isLoggedIn = false 
+let state = null
+//boolean = > true/false
+
+//null  => standalone value
+
+//undefined => 
+
+console.log(typeof undefined);//undefined
+console.log(typeof null)//object
+
---
+++ 01_basics/03_typeConversion.js
@@ -0,0 +1,28 @@
+// let score = false;
+
+// console.log(typeof score);
+// console.log(typeof score);
+
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+//"33" => 33
+//'33aa '=> NaN
+//true=>1 ; false=>0
+
+// let isLoggedIn = "amit";
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+
+//1=> true  0=>false 
+//"" => false 
+// "hi" => true
+
+let someNumber=33
+
+let stringNumber=String(someNumber)
+console.log(typeof stringNumber)
+console.log(stringNumber);
